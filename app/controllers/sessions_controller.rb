b0771d3e9f064f424936ee2583e6cdefe4d43be7
helloamandamurphy <helloamandamurphy@gmail.com>
2019-03-12 15:20:02 -0400

    Modify sessions_controller.rb to accept email for login and redirect to /posts page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,9 @@ class SessionsController < ApplicationController
   get '/login' do
     erb :"sessions/login.html"
   end
+
+  post '/sessions' do
+    session[:email] = params[:email]
+    redirect '/posts'
+  end
 end
