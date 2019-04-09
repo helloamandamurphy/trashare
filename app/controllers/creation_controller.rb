be0e943c52aa6abacff950b5374738d8acb5ea34
helloamandamurphy <helloamandamurphy@gmail.com>
2019-04-08 23:58:23 -0400

    Add basic CRUD action methods to creation and request controllers; rename make to create in views

--- app/controllers/creation_controller.rb
+++ app/controllers/creation_controller.rb
@@ -1,2 +1,42 @@
 class CreationController < ApplicationController
+
+  get '/creations/new' do
+    #if !logged_in?
+      #redirect "/login"
+    #else
+      erb :"create/new_creation"
+    #end
+  end
+
+  #post '/creations' do
+    #@creation = Creation.new(:key params(key))
+  #end
+
+  get '/creations' do
+    erb :"create/index"
+  end
+
+  get '/creations/:id' do
+    #if !logged_in?
+      #redirect "/login"
+    #else
+      #if @creation = current_user.creation.find_by(params[:id])
+        erb :"create/show_creation"
+      #else
+        #redirect '/creations'
+      #end
+    #end
+  end
+
+  get '/creations/:id/edit' do
+    #if !logged_in?
+      #redirect "/login"
+    #else
+      #if @creation = current_user.creation.find_by(params[:id])
+        erb :"create/edit_creation"
+      #else
+        #redirect '/creations'
+      #end
+    #end
+  end
 end
--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,2 +1,42 @@
 class RequestController < ApplicationController
+
+  get '/requests/new' do
+    #if !logged_in?
+      #redirect "/login"
+    #else
+      erb :"request/new_request"
+    #end
+  end
+
+  #post '/creations' do
+    #@request = Request.new(:key params(key))
+  #end
+
+  get '/requests' do
+    erb :"request/index"
+  end
+
+  get '/requests/:id' do
+    #if !logged_in?
+      #redirect "/login"
+    #else
+      #if @request = current_user.request.find_by(params[:id])
+        erb :"request/show_request"
+      #else
+        #redirect '/requests'
+      #end
+    #end
+  end
+
+  get '/requests/:id/edit' do
+    #if !logged_in?
+      #redirect "/login"
+    #else
+      #if @request = current_user.request.find_by(params[:id])
+        erb :"request/edit_request"
+      #else
+        #redirect '/requests'
+      #end
+    #end
+  end
 end
